259a0d2ddf01042e3795777714e6659a19519462
genesissiana <perezgenesisa@gmail.com>
2021-09-28 13:15:08 -0700

    signup form, add new user

--- client/src/pages/SearchBooks.js
+++ client/src/pages/SearchBooks.js
@@ -1,7 +1,9 @@
 import React, { useState, useEffect } from 'react';
+import { useMutation } from '@apollo/client';
 import { Jumbotron, Container, Col, Form, Button, Card, CardColumns } from 'react-bootstrap';
 
 import Auth from '../utils/auth';
+import { SAVE_BOOK } from '../utils/mutations';
 import { saveBook, searchGoogleBooks } from '../utils/API';
 import { saveBookIds, getSavedBookIds } from '../utils/localStorage';
 
