bdfb491c09c4056179fd6a45f9324e344f8fbbb1
genesissiana <perezgenesisa@gmail.com>
2021-09-28 12:27:26 -0700

    install Apollo to server, gql to client

--- server/server.js
+++ server/server.js
@@ -1,4 +1,7 @@
 const express = require('express');
+const { ApolloServer } = require('apollo-server-express');
+const { typeDefs, resolvers } = require('./schemas');
+const { authMiddleWare } = require('./utils/auth');
 const path = require('path');
 const db = require('./config/connection');
 const routes = require('./routes');
@@ -6,10 +9,17 @@ const routes = require('./routes');
 const app = express();
 const PORT = process.env.PORT || 3001;
 
+server = new ApolloServer({
+  typeDefs,
+  resolvers,
+  context: authMiddleWare
+});
+
+server.applyMiddleware({ app });
+
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
-// if we're in production, serve client/build as static assets
 if (process.env.NODE_ENV === 'production') {
   app.use(express.static(path.join(__dirname, '../client/build')));
 }
@@ -17,5 +27,8 @@ if (process.env.NODE_ENV === 'production') {
 app.use(routes);
 
 db.once('open', () => {
-  app.listen(PORT, () => console.log(`🌍 Now listening on localhost:${PORT}`));
+  app.listen(PORT, () => {
+    console.log(`🌍 Now listening on localhost:${PORT}`);
+    console.log(`Use GraphQL at http://localhost:${PORT}${server.graphqlPath}`)
+  });
 });
