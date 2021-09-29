6366d59da880fdccf61c51c9011599894c0432ca
genesissiana <perezgenesisa@gmail.com>
2021-09-28 23:46:30 -0700

    update server typos

--- server/server.js
+++ server/server.js
@@ -1,21 +1,28 @@
 const express = require('express');
+
+const path = require('path');
+const db = require('./config/connection');
+// const routes = require('./routes');
+
 const { ApolloServer } = require('apollo-server-express');
 const { typeDefs, resolvers } = require('./schemas');
 const { authMiddleWare } = require('./utils/auth');
-const path = require('path');
-const db = require('./config/connection');
-const routes = require('./routes');
 
 const app = express();
 const PORT = process.env.PORT || 3001;
 
-server = new ApolloServer({
+const server = new ApolloServer({
   typeDefs,
   resolvers,
   context: authMiddleWare
 });
 
-server.applyMiddleware({ app });
+async function startServer() {
+  await server.start();
+  server.applyMiddleware({ app });
+}
+startServer();
+
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
@@ -24,7 +31,9 @@ if (process.env.NODE_ENV === 'production') {
   app.use(express.static(path.join(__dirname, '../client/build')));
 }
 
-app.use(routes);
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, '../client/build/index.html'));
+});
 
 db.once('open', () => {
   app.listen(PORT, () => {
